9addf25fef659d53953df00ca40e6992e403990e
limao0706 <limao0706@thundersoft.com>
2016-09-21 18:32:06 +0800

    add input

--- src/pages/loginPage.vue
+++ src/pages/loginPage.vue
@@ -1,5 +1,43 @@
 <template>
     <div id="login-page">
-        login page
+        <bs-input
+            :value.sync="input"
+            label="Username"
+            help="Only allows lowercase letters and numbers."
+            error="Insert username"
+            pattern="^[a-z][a-z0-9]+$"
+            :mask="mask"
+            minlength="5"
+            icon>
+        </bs-input>
+        <bs-input
+            :value.sync="input"
+            label="Password"
+            help="Only allows lowercase letters and numbers."
+            error="Insert username"
+            pattern="^[a-z][a-z0-9]+$"
+            :mask="mask"
+            minlength="5"
+            icon>
+        </bs-input>
     </div>
 </template>
+<script>
+    var Input = require('vue-strap').input;
+
+    module.exports = {
+        data: function() {
+            return {
+                input: ''
+            }
+        },
+        methods: {
+            mask: function (value) {
+                return value.toLowerCase();
+            }
+        },
+        components: {
+            'bs-input': Input
+        }
+    };
+</script>
