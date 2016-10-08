7d9cf225127ddf57ba9b3b0edf7768ff99ddbede
limao0706 <limao0706@thundersoft.com>
2016-10-08 18:44:59 +0800

    add mutation-type

---
+++ src/store/mutation-types.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'ADD_TO_CART': 'addToCart',
+    'CHECKOUT_REQUEST': 'checkoutRequest',
+    'CHECKOUT_SUCCESS': 'checkoutSuccess',
+    'CHECKOUT_FAILURE': 'checkoutFailture',
+    'RECEIVE_PRODUCTS': 'receiveProducts'
+};
